4383a6f94200eb8a3fae51bcad258a9b2dc24c48
Martina Ferrari <martinaferrari98@gmail.com>
2021-06-28 14:16:44 -0300

    Definicion de estructura celda

--- progSecuencial.c
+++ progSecuencial.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 
+#define Invalido -1
+#define Blanco 0
+#define Azul 1
+#define Rojo 2
+#define Naranja 3
+#define Verde 4
+
+#define Joven 0
+#define Adulto 1
+#define Viejo 2
+
+#define Si 1
+#define No 0
+
+typedef struct celda{
+    int estado; //Blanco, Azul, Rojo, Naranja, Verde
+    int edad; //Joven, Adulto, Viejo
+    int heridasAbiertas; //Si, No
+    int tiempo; //Tiempo transcurrido desde la ultima actualizacion de estado
+}celda;
+
 int main() {
     printf("Hello, World!\n");
     return 0;
 }
+
