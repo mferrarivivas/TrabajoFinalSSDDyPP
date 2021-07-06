9d01253d453d8ac89bee80587dde349444e60f73
Lucas <lucasjimnez@gmail.com>
2021-07-06 17:14:45 -0300

    separo script

--- script.sh
+++ script.sh
@@ -7,10 +7,11 @@ printf "\nMatriz 200 X 200\n"
 
 sleep 2
 
+
 counter=1
 while [ $counter -le 10 ]
 do
-./progSecuencial 200 120 
+./progSecuencial 200 2000 
 sleep 1
 ((counter++))
 done
@@ -22,7 +23,7 @@ sleep 2
 counter=1
 while [ $counter -le 10 ]
 do
-./progSecuencial 800 120 
+./progSecuencial 800 1000 
 sleep 1
 ((counter++))
 done
@@ -34,121 +35,7 @@ sleep 2
 counter=1
 while [ $counter -le 10 ]
 do
-./progSecuencial 1500 120 
-sleep 1
-((counter++))
-done
-
-printf "\t\nMPI CON 2 PROCESOS\n\n"
-
-printf "\nMatriz 200 X 200\n"
-
-sleep 2
-
-counter=1
-while [ $counter -le 10 ]
-do
-mpirun -np 2 -f ./hostfile ./progMPI 200 120
-sleep 1
-((counter++))
-done
-
-printf "\nMatriz 800 X 800\n"
-
-sleep 2
-
-counter=1
-while [ $counter -le 10 ]
-do
-mpirun -np 2 -f ./hostfile ./progMPI 800 120 
-sleep 1
-((counter++))
-done
-
-printf "\nMatriz 1500 X 1500\n"
-
-sleep 2
-
-counter=1
-while [ $counter -le 10 ]
-do
-mpirun -np 2 -f ./hostfile ./progMPI 1500 120  
-sleep 1
-((counter++))
-done
-
-printf "\t\nMPI CON 4 PROCESOS\n"
-
-printf "\nMatriz 200 X 200\n"
-
-sleep 2
-
-counter=1
-while [ $counter -le 10 ]
-do
-mpirun -np 4 -f ./hostfile ./progMPI 200 120
-sleep 1
-((counter++))
-done
-
-printf "\nMatriz 800 X 800\n"
-
-sleep 2
-
-counter=1
-while [ $counter -le 10 ]
-do
-mpirun -np 4 -f ./hostfile ./progMPI 800 120 
-sleep 1
-((counter++))
-done
-
-printf "\nMatriz 1500 X 1500\n"
-
-sleep 2
-
-counter=1
-while [ $counter -le 10 ]
-do
-mpirun -np 4 -f ./hostfile ./progMPI 1500 120  
-sleep 1
-((counter++))
-done
-
-printf "\t\nMPI CON 10 PROCESOS\n"
-
-printf "\nMatriz 200 X 200\n"
-
-sleep 2
-
-counter=1
-while [ $counter -le 10 ]
-do
-mpirun -np 10 -f ./hostfile ./progMPI 200 120
-sleep 1
-((counter++))
-done
-
-printf "\nMatriz 800 X 800\n"
-
-sleep 2
-
-counter=1
-while [ $counter -le 10 ]
-do
-mpirun -np 10 -f ./hostfile ./progMPI 800 120 
-sleep 1
-((counter++))
-done
-
-printf "\nMatriz 1500 X 1500\n"
-
-sleep 2
-
-counter=1
-while [ $counter -le 10 ]
-do
-mpirun -np 10 -f ./hostfile ./progMPI 1500 120  
+./progSecuencial 1500 500 
 sleep 1
 ((counter++))
 done
@@ -162,7 +49,7 @@ sleep 2
 counter=1
 while [ $counter -le 10 ]
 do
-./progOpenMP 200 2 120
+./progOpenMP 200 2000 2
 sleep 1
 ((counter++))
 done
@@ -174,7 +61,7 @@ sleep 2
 counter=1
 while [ $counter -le 10 ]
 do
-./progOpenMP 800 2 120 
+./progOpenMP 800 1000 2 
 sleep 1
 ((counter++))
 done
@@ -186,7 +73,7 @@ sleep 2
 counter=1
 while [ $counter -le 10 ]
 do
-./progOpenMP 1500 2 120   
+./progOpenMP 1500 500 2   
 sleep 1
 ((counter++))
 done
@@ -200,7 +87,7 @@ sleep 2
 counter=1
 while [ $counter -le 10 ]
 do
-./progOpenMP 200 4 120
+./progOpenMP 200 2000 4
 sleep 1
 ((counter++))
 done
@@ -212,7 +99,7 @@ sleep 2
 counter=1
 while [ $counter -le 10 ]
 do
-./progOpenMP 800 4 120 
+./progOpenMP 800 1000 4
 sleep 1
 ((counter++))
 done
@@ -224,7 +111,7 @@ sleep 2
 counter=1
 while [ $counter -le 10 ]
 do
-./progOpenMP 1500 4 120   
+./progOpenMP 1500 500 4   
 sleep 1
 ((counter++))
 done
@@ -238,7 +125,7 @@ sleep 2
 counter=1
 while [ $counter -le 10 ]
 do
-./progOpenMP 200 8 120
+./progOpenMP 200 2000 8
 sleep 1
 ((counter++))
 done
@@ -250,7 +137,7 @@ sleep 2
 counter=1
 while [ $counter -le 10 ]
 do
-./progOpenMP 800 8 120 
+./progOpenMP 800 1000 8 
 sleep 1
 ((counter++))
 done
@@ -262,237 +149,7 @@ sleep 2
 counter=1
 while [ $counter -le 10 ]
 do
-./progOpenMP 1500 8 120   
+./progOpenMP 1500 500 8   
 sleep 1
 ((counter++))
 done
-
-printf "\t\nHIBRIDO CON 2 PROCESOS Y 2 THREADS\n"
-
-printf "\nMatriz 200 X 200\n"
-
-sleep 2
-
-counter=1
-while [ $counter -le 10 ]
-do
-mpirun -np 2 -f ./hostfile ./progHibrido 200 2 120
-sleep 1
-((counter++))
-done
-
-printf "\nMatriz 800 X 800\n"
-
-sleep 2
-
-counter=1
-while [ $counter -le 10 ]
-do
-mpirun -np 2 -f ./hostfile ./progHibrido 800 2 120 
-sleep 1
-((counter++))
-done
-
-printf "\nMatriz 1500 X 1500\n"
-
-sleep 2
-
-counter=1
-while [ $counter -le 10 ]
-do
-mpirun -np 2 -f ./hostfile ./progHibrido 1500 2 120    
-sleep 1
-((counter++))
-done
-
-printf "\t\nHIBRIDO CON 2 PROCESOS Y 4 THREADS\n"
-
-printf "\nMatriz 200 X 200\n"
-
-sleep 2
-
-counter=1
-while [ $counter -le 10 ]
-do
-mpirun -np 2 -f ./hostfile ./progHibrido 200 4 120
-sleep 1
-((counter++))
-done
-
-printf "\nMatriz 800 X 800\n"
-
-sleep 2
-
-counter=1
-while [ $counter -le 10 ]
-do
-mpirun -np 2 -f ./hostfile ./progHibrido 800 4 120 
-sleep 1
-((counter++))
-done
-
-printf "\nMatriz 1500 X 1500\n"
-
-sleep 2
-
-counter=1
-while [ $counter -le 10 ]
-do
-mpirun -np 2 -f ./hostfile ./progHibrido 1500 4 120    
-sleep 1
-((counter++))
-done
-
-printf "\t\nHIBRIDO CON 4 PROCESOS Y 2 THREADS\n"
-
-printf "\nMatriz 200 X 200\n"
-
-sleep 2
-
-counter=1
-while [ $counter -le 10 ]
-do
-mpirun -np 4 -f ./hostfile ./progHibrido 200 2 120
-sleep 1
-((counter++))
-done
-
-printf "\nMatriz 800 X 800\n"
-
-sleep 2
-
-counter=1
-while [ $counter -le 10 ]
-do
-mpirun -np 4 -f ./hostfile ./progHibrido 800 2 120 
-sleep 1
-((counter++))
-done
-
-printf "\nMatriz 1500 X 1500\n"
-
-sleep 2
-
-counter=1
-while [ $counter -le 10 ]
-do
-mpirun -np 4 -f ./hostfile ./progHibrido 1500 2 120    
-sleep 1
-((counter++))
-done
-
-printf "\t\nHIBRIDO CON 4 PROCESOS Y 4 THREADS\n"
-
-printf "\nMatriz 200 X 200\n"
-
-sleep 2
-
-counter=1
-while [ $counter -le 10 ]
-do
-mpirun -np 4 -f ./hostfile ./progHibrido 200 4 120
-sleep 1
-((counter++))
-done
-
-printf "\nMatriz 800 X 800\n"
-
-sleep 2
-
-counter=1
-while [ $counter -le 10 ]
-do
-mpirun -np 4 -f ./hostfile ./progHibrido 800 4 120 
-sleep 1
-((counter++))
-done
-
-printf "\nMatriz 1500 X 1500\n"
-
-sleep 2
-
-counter=1
-while [ $counter -le 10 ]
-do
-mpirun -np 4 -f ./hostfile ./progHibrido 1500 4 120    
-sleep 1
-((counter++))
-done
-
-printf "\t\nHIBRIDO CON 10 PROCESOS Y 2 THREADS\n"
-
-printf "\nMatriz 200 X 200\n"
-
-sleep 2
-
-counter=1
-while [ $counter -le 10 ]
-do
-mpirun -np 10 -f ./hostfile ./progHibrido 200 2 120
-sleep 1
-((counter++))
-done
-
-printf "\nMatriz 800 X 800\n"
-
-sleep 2
-
-counter=1
-while [ $counter -le 10 ]
-do
-mpirun -np 10 -f ./hostfile ./progHibrido 800 2 120 
-sleep 1
-((counter++))
-done
-
-printf "\nMatriz 1500 X 1500\n"
-
-sleep 2
-
-counter=1
-while [ $counter -le 10 ]
-do
-mpirun -np 10 -f ./hostfile ./progHibrido 1500 2 120    
-sleep 1
-((counter++))
-done
-
-printf "\t\nHIBRIDO CON 10 PROCESOS Y 4 THREADS\n"
-
-printf "\nMatriz 200 X 200\n"
-
-sleep 2
-
-counter=1
-while [ $counter -le 10 ]
-do
-mpirun -np 10 -f ./hostfile ./progHibrido 200 4 120
-sleep 1
-((counter++))
-done
-
-printf "\nMatriz 800 X 800\n"
-
-sleep 2
-
-counter=1
-while [ $counter -le 10 ]
-do
-mpirun -np 10 -f ./hostfile ./progHibrido 800 4 120 
-sleep 1
-((counter++))
-done
-
-printf "\nMatriz 1500 X 1500\n"
-
-sleep 2
-
-counter=1
-while [ $counter -le 10 ]
-do
-mpirun -np 10 -f ./hostfile ./progHibrido 1500 4 120    
-sleep 1
-((counter++))
-done
-
-printf "\n\n FIN DE SCRIPT\n\n"
