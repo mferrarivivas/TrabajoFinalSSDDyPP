6bbba64f0bf7caf2125a02fe82957cadc439c20d
Lucas <lucasjimnez@gmail.com>
2021-07-06 23:36:47 -0300

    qwerty

--- scriptSecuencial.sh
+++ scriptSecuencial.sh
@@ -1,32 +1,7 @@
 #!/bin/bash
 # Basic while loop
 
-
-for n in {4..10}
-do
-./progOpenMP 1500 150 4   
-
-
-done
-
-printf "\t\nOPNEMP CON 8 THREADS\n"
-
-printf "\nMatriz 200 X 200\n"
-
-sleep 2
-
-for n in {1..10}
-do
-./progOpenMP 200 150 8
-
-
-done
-
-printf "\nMatriz 800 X 800\n"
-
-sleep 2
-
-for n in {1..10}
+for n in {2..10}
 do
 ./progOpenMP 800 150 8 
 
